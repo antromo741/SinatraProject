4f8c56161dca05cc2b78620943311748e42c0f03
antromo <anthonyroma741@gmail.com>
2020-09-17 18:51:23 -0400

    very strage things going on with my repo and my last commit, ill be so upset if I lose it all

--- SinatraProject/app/models/ferret.rb
+++
@@ -1,6 +0,0 @@
-class Ferret < ActiveRecord::Base
-  belongs_to :user
-  validates :name, presence: true
-  validates :hair, presence: true
-  #validates :dropff, presence: true
-end
--- SinatraProject/db/migrate/20200917215457_change_value.rb
+++
@@ -1,5 +0,0 @@
-class ChangeValue < ActiveRecord::Migration
-  def change
-    change_column :ferrets, :picked_up, :boolean, :default => false
-  end
-end
